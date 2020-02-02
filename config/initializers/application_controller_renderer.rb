45d5aeae7d6f964089d33e42e9b898a59121809e
s-ito-coder <holyarrow109@gmail.com>
2020-02-02 14:38:35 +0900

    [FINISH] 6-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '141ee697f681a696fe4f781664e1bf80a07cebef190c20b661e20e002a69e73f45a79c33bcc57c44df322ecc21f12eed7579112e374e0d2206ea710a1ef810ee'
